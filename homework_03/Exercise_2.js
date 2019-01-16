cff601d38c7e0adfcb2d1f8601641ecd01a47797
angarag <mars0411@gmail.com>
2019-01-16 14:25:59 -0600

    Exercise 2 is done

---
+++ homework_03/Exercise_2.js
@@ -0,0 +1,23 @@
+var EventEmitter = require('events');
+
+class Gym extends EventEmitter {
+    constructor() {
+        super();
+        this.message = 'Hi!';
+        setInterval(()=>{
+            this.emit('boom', 'MUM student');    
+        },1000);
+
+    }
+    feedBoom(message) {
+        this.on('boom', function (name) {
+            if(message)
+            console.log(message);
+            else
+            console.log(`Welcome ${name}`);
+        }) 
+    }
+}
+
+var gym = new Gym();
+gym.feedBoom('Athlete is working out');
