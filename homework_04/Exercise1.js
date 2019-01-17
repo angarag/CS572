1dc21dab61ff2a750e2304c0e2998bd16adef00c
angarag <mars0411@gmail.com>
2019-01-17 14:05:38 -0600

    HW4-Exercise1 is done

---
+++ homework_04/Exercise1.js
@@ -0,0 +1,54 @@
+(() => {
+
+    const rxjs = require('rxjs');
+    const { of, from, Observable } = rxjs;
+    const { map, filter, mergeMap } = require('rxjs/operators');//rxjs.operators;
+    const os = require('os');
+    function checkSystem(ram = 4, core = 2) {
+        if (rxjs === null || rxjs === undefined) {
+            console.log(`Please install RXJS module by running
+        npm install rxjs
+        `)
+        }
+        else {
+            let result = true;
+            const message = {
+                init_message: `Checking your system...`,
+                memory_not_enough: `This app needs at least 4 GB of RAM`,
+                cpu_not_enough: `Processor is not supported`,
+                ok: 'System is checked successfully'
+            }
+            console.log(message.init_message);
+            const hello = Observable.create(function (observer) {
+                observer.next({
+                    cpu: Math.ceil(os.totalmem() / 1024 / 1024 / 1024)
+                });
+                observer.next({
+                    ram: os.cpus()
+                });
+            });
+
+            let cpu_subscribtion = hello.subscribe(val => {
+                if (val.cpu && val.cpu < (core + 1)) {
+                    console.log(message.cpu_not_enough);
+                    result = false;
+                }
+                //console.log('cpu_check');
+            });
+            cpu_subscribtion.unsubscribe();
+            let ram_subscription = hello.subscribe(val => {
+                if (val.ram && val.ram < (ram)) {
+                    console.log(message.memory_not_enough);
+                    result = false;
+                }
+                //console.log('ram_check');
+            })
+            ram_subscription.unsubscribe();
+
+            if (result)
+                console.log(message.ok);
+
+        }
+    }
+    checkSystem();
+})()
