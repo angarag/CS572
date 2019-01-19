3e3efe54853038ad39a1e7de4391b8ce47813f45
angarag <mars0411@gmail.com>
2019-01-18 23:27:36 -0600

    Users rendered in html

--- homework_05/Exercise1.js
+++ homework_05/Exercise1.js
@@ -5,10 +5,12 @@ const axios = require('axios');
 const cacheControl = require('express-cache-controller')
 const url = require('url');
 const paginate = require('express-paginate');
-
+const path = require('path')
 const port = process.env.port || 3000;
+const cache_duration = 86400; //1 day
 var app = express();
 var router = express.Router();
+var mcache = require('memory-cache');
 
 //Setup
 app.set('trust proxy',true);
@@ -19,11 +21,28 @@ app.set('query parser','simple');
 
 app.use(cacheControl({
     private: true,
-    maxAge: 86400,
+    maxAge: cache_duration,
 }));
 app.use(paginate.middleware(10, 50));
 
-app.set('');
+var cache = (duration) => {
+    return (req, res, next) => {
+      let key = '__express__' + (req.originalUrl || req.url);// + JSON.stringify(req.headers)['if-none-match']
+      console.log(JSON.stringify(req.headers)['if-none-match']);
+      let cachedBody = mcache.get(key)
+      if (cachedBody) {
+        res.send(cachedBody)
+        return
+      } else {
+        res.sendResponse = res.send
+        res.send = (body) => {
+          mcache.put(key, body, duration * 1000);
+          res.sendResponse(body)
+        }
+        next()
+      }
+    }
+  }
 /* GET home page. */
 app.get('/favicon.ico',(req,res)=>{
     const url_obj = url.parse(req.url, true);
@@ -31,28 +50,36 @@ app.get('/favicon.ico',(req,res)=>{
         res.end();
 
 })
-app.get('/users',(req,res)=>{
+//cache = 10 seconds
+app.get('/users',cache(cache_duration/24/360/2), (req,res)=>{
     const obj = {
         message:'Error occured in parsing',
         course:'MWA'
     }
     res.set({
-        'content-type':'application/json',
+        'content-type':'text/html',
         'warning':'this is MWA course',
         'Last-Modified': (new Date()).toUTCString(),
-        'Expires':(new Date(new Date().setDate(new Date().getDate() + 1))).toUTCString()
+        'Expires':(new Date(new Date().setDate(new Date().getDate() + cache_duration/3600/24))).toUTCString()
     })
     const mars = async ()=> {
         try{
             const result = await axios.get('https://randomuser.me/api/?results=10');
             console.log(result.data.results);
-            res.jsonp(result.data.results);
+            const html_data = result.data.results.map(
+                item => `<tr><td>${item.login.username}</td><td>${item.email}</td></tr>`
+              );
+              const table=`<table><tr><h3>Users</h3></tr>${html_data}</table>`;
+             res.write(table);
         }catch(error){
             console.log('Error occured',error);
             res.jsonp(message)
         }
     };
-    mars();
+    setTimeout(() => {
+        mars();
+    }, 1000) //setTimeout was used to simulate a slow processing request
+    
    
 })
 
