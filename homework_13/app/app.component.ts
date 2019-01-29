1b3f84da0faca9176c0f6c729ce6f8e095ee63bf
angarag <mars0411@gmail.com>
2019-01-28 21:42:02 -0600

    HW13: Angular: Played with ViewChild, ContentChild and Component LifeCycle events

--- homework_13/app/app.component.ts
+++ homework_13/app/app.component.ts
@@ -1,21 +1,35 @@
 import { Component } from '@angular/core';
+import {ViewChild, AfterViewInit, ngAfterContentInit,OnInit } from '@angular/core';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
   template: `
-  <app-counter [counter]="parent_counter" (counterChange)="receiveEmit($event)"></app-counter>
+  <app-counter [counter]="parent_counter"
+   [val1]="parent_val"
+   val2="{{parent_val}}"
+   val3="3"
+    (counterChange)="receiveEmit($event)">
+    <span #insideNGContent>NG_Content_defined_by_Parent</span>
+    
+  </app-counter>
 
   <p>Component Counter value: {{parent_counter}}</p>
   `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, ngAfterContentInit {
   title = 'angularProject';
   parent_counter:number;
-
+  parent_val:number;
+  @ViewChild(CounterComponent) messageViewChild: CounterComponent;
   constructor(){
     this.parent_counter=1;
+    this.parent_val=12;
   }
+  ngAfterContentInit() {
+    this.messageViewChild.counterValue = 0;//'Passed as View Child';
+}
   receiveEmit(x){
     this.parent_counter=x;
       console.log('Received:',x)
--- homework_13/app/counter/counter.component.ts
+++ homework_13/app/counter/counter.component.ts
@@ -1,39 +1,63 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
+import {ViewChildren , QueryList , AfterViewInit, ContentChild, OnChanges} from '@angular/core';
 @Component({
   selector: 'app-counter',
   template:
    `<p>
-   Enter counter value: <input type="text" [value]="counter" (keyup)=emitValue(child_input.value) #child_input/>
+   Enter counter value: <input type="text" [value]="counter"
+    (input)="setValue($event.target.value)"
+    (keyup)=emitValue(child_input.value) #child_input/>
    </p>
     <p>
     <button (click)="changeValue(-1)">-</button>
-    <ng-content></ng-content>{{counterValue}}
+    {{counterValue}}
       <button (click)="changeValue(+1)">+</button>
       </p>
+      <ng-content></ng-content>
       
   `,
-  styles: []
+  styles: [`p{color:red}`]
 })
-export class CounterComponent{
+export class CounterComponent implements OnChanges{
   counterValue: number;
   @Input() counter:number;
+  @Input() val1;
+  @Input() val2:number;
+  @Input() val3;
+@ContentChild('insideNGContent') ng_content;
+
   @Output() counterChange;
   constructor() {
     this.counterChange= new EventEmitter();
-    this.counterValue = 0;
+    //this.counterValue = 0;
   }
 
+  //Option#3 - Not finished
+  ngOnChanges(changes){
+    console.log(this.ng_content)
+    console.log(`the above one is NG Content, the below one is Component input onChanges:`)
+    console.log(changes);//.counter.currentValue)
+  }
+  //Option#2
+  setValue(val){
+    let x=parseInt(val);
+    this.counterValue=x;
+    this.counterChange.emit(x);
+  }
   changeValue(val){
     this.counterValue=this.counterValue+val;
     this.counterChange.emit(this.counterValue);
     return this;
   }
 
+    /*Option#1
+    Alternative option to (input)="setValue($event.target.value)"
+    this.counterValue=x; 
+    */
   emitValue(x): void{
-    x=parseInt(x);
-    this.counterValue=x;
+    console.log(`Input types received: Val1: ${typeof this.val1}, Val2: ${typeof this.val2}, Val3: ${typeof this.val3}`)
     console.log('input onChange', x)
+    x=parseInt(x);
     this.counterChange.emit(x)
   }
 
